5fbe2171b194263fbeea4f71eb7e0d8655b849b6
jhon <lopiccoloottavio@gmail.com>
2021-01-21 17:29:36 +0100

    s

--- src/main/java/com/example/arcana/model/User.java
+++ src/main/java/com/example/arcana/model/User.java
@@ -21,7 +21,7 @@ import static javax.persistence.GenerationType.IDENTITY;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-//@Table(name = "arcana_users", schema = "arcana")
+@Table(name = "arcana_users", schema = "de1rahdv1sqjnb")
 public class User implements Serializable{
     @Id
     @GeneratedValue(strategy = IDENTITY)
